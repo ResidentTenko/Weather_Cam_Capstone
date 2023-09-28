2a4a3b5147f49ea6b1e54852968ae6dad66f050a
Fidel Ojimba <fidelojimba@gmail.com>
2023-09-28 19:03:30 -0400

    Assets and Yaml Added

--- weather_cam/lib/main.dart
+++ weather_cam/lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const SearchTestField(),
+        home: const WeatherTest(),
       ),
     );
   }
